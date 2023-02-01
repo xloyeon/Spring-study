c3ea06c4238154aada5bd4d4eeaf4533362ede49
xloyeon <496300@naver.com>
2023-02-01 20:37:57 +0900

    feat: 스프링 입문 안승연 강의 연습 project 추가

---
+++ week1/week1_안승연/스프링핵심입문/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,44 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    //save할 때 저장할 곳 Map 이용해서 저장(회원 id, 멤버)
+    private static Map<Long, Member> store = new HashMap<>();
+
+    //sequence는 0,1,2...등 키 값
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+}
