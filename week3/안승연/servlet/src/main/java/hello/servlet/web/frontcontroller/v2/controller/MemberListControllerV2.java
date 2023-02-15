f77c5a31c4099e5fce994d96ee628046c1d017e4
xloyeon <496300@naver.com>
2023-02-15 20:05:53 +0900

    week3 : 안승연 서블릿, mvc 프로젝트 추가

---
+++ week3/안승연/servlet/src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberListControllerV2.java
@@ -0,0 +1,27 @@
+package hello.servlet.web.frontcontroller.v2.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v2.ControllerV2;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class MemberListControllerV2 implements ControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Member> all = memberRepository.findAll();
+
+        //model에 데이터 담기
+        request.setAttribute("members", all);
+        return new MyView("/WEB-INF/views/members.jsp");
+    }
+}
